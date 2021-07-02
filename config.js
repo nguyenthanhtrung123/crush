b0b2e9df71f2960f9638a33650b32f02704a898d
nguyenthanhtrung123 <72900081+nguyenthanhtrung123@users.noreply.github.com>
2021-07-02 09:48:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Hiếu à!',
+    introTitle: 'Hiếu con ông trung  à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
